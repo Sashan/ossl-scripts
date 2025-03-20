eb72a073be5f5272a7bc3e4a6b3f8f28fe8adebd
sashan <anedvedicky@gmail.com>
2025-03-20 16:21:07 +0100

    there are three modes of operation: 	1 - (MPROFILE_MODE=1) collect just basic stats 	2 - (MPROFILE_MODE=2) collect allocation chains 	3 - (MPROFILE_MODE=3) collect allocation chains with back traces

--- mprofile/init.c
+++ mprofile/init.c
@@ -13,20 +13,54 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
+#include <sys/atomic.h>
+
 #include <openssl/crypto.h>
 
 #include "mprofile.h"
 
-static void *mp_CRYPTO_malloc(size_t, const char *, int);
-static void mp_CRYPTO_free(void *, const char *, int);
-static void *mp_CRYPTO_realloc(void *, size_t, const char *, int);
+static void *mp_CRYPTO_malloc_stats(unsigned long, const char *, int);
+static void mp_CRYPTO_free_stats(void *, const char *, int);
+static void *mp_CRYPTO_realloc_stats(void *, unsigned long, const char *, int);
+
+static void *mp_CRYPTO_malloc_trace(unsigned long, const char *, int);
+static void mp_CRYPTO_free_trace(void *, const char *, int);
+static void *mp_CRYPTO_realloc_trace(void *, unsigned long, const char *, int);
+
+static void *mp_CRYPTO_malloc_trace_with_stack(unsigned long, const char *, int);
+static void mp_CRYPTO_free_trace_with_stack(void *, const char *, int);
+static void *mp_CRYPTO_realloc_trace_with_stack(void *, unsigned long, const char *, int);
 
 struct memhdr {
 	size_t		mh_size;
 	uint64_t	mh_chk;
 };
+
+#define	MS_TOTAL_ALLOCATED	"\"total_allocated_sz\""
+#define	MS_TOTAL_RELEASED	"\"total_released_sz\""
+#define	MS_ALLOCS		"\"allocs\""
+#define	MS_RELEASES		"\"releases\""
+#define	MS_REALLOCS		"\"reallocs\""
+#define	MS_MAX_MEM		"\"max_mem\""
+#define	MS_TSTART		"\"tstart\""
+#define	MS_TFINISH		"\"tfinish\""
+#define	MS_SEC			"\"sec\""
+#define	MS_NSEC			"\"nsec\""
+static struct memstats {
+	uint64_t	ms_total_allocated;
+	uint64_t	ms_total_released;
+	uint64_t	ms_reallocs;
+	uint64_t	ms_allocs;
+	uint64_t	ms_free;
+	uint64_t	ms_max;
+	uint64_t	ms_current;
+	struct timespec	ms_start;
+	struct timespec	ms_finish;
+} ms;
 
 static pthread_key_t mp_pthrd_key;
 
@@ -41,7 +75,41 @@ merge_profile(void *void_mprof)
 }
 
 static void
-save_profile(void)
+save_stats(void)
+{
+	FILE	*f;
+	char	*fname;
+
+	clock_gettime(CLOCK_REALTIME, &ms.ms_finish);
+
+	fname = getenv("MPROFILE_OUTF");
+	if (fname == NULL)
+		return;
+
+	f = fopen(fname, "w");
+	if (f == NULL)
+		return;
+
+	fprintf(f, "{\n");
+	fprintf(f, "\t%s : %llu,\n", MS_TOTAL_ALLOCATED, ms.ms_total_allocated);
+	fprintf(f, "\t%s : %llu,\n", MS_TOTAL_RELEASED, ms.ms_total_released);
+	fprintf(f, "\t%s : %llu,\n", MS_ALLOCS, ms.ms_allocs);
+	fprintf(f, "\t%s : %llu,\n", MS_RELEASES, ms.ms_free);
+	fprintf(f, "\t%s : %llu,\n", MS_REALLOCS, ms.ms_reallocs);
+	fprintf(f, "\t%s : %llu,\n", MS_MAX_MEM, ms.ms_max);
+	fprintf(f, "\t%s : {\n", MS_TSTART);
+	fprintf(f, "\t\t%s : %llu,\n", MS_SEC, ms.ms_start.tv_sec);
+	fprintf(f, "\t\t%s : %ld,\n", MS_NSEC, ms.ms_start.tv_nsec);
+	fprintf(f, "\t},\n");
+	fprintf(f, "\t%s : {\n", MS_TFINISH);
+	fprintf(f, "\t\t%s : %llu,\n", MS_SEC, ms.ms_finish.tv_sec);
+	fprintf(f, "\t\t%s : %ld,\n", MS_NSEC, ms.ms_finish.tv_nsec);
+	fprintf(f, "\t}\n");
+	fprintf(f, "}");
+}
+
+static void
+save_profile_trace(void)
 {
 	pthread_key_delete(mp_pthrd_key);
 	mprofile_merge();
@@ -77,18 +145,309 @@ get_mprofile(void)
 	return (mp);
 }
 
+static void
+init_stats(void)
+{
+	clock_gettime(CLOCK_REALTIME, &ms.ms_start);
+	CRYPTO_set_mem_functions(mp_CRYPTO_malloc_stats,
+	    mp_CRYPTO_realloc_stats,
+	    mp_CRYPTO_free_stats);
+	atexit(save_stats);
+}
+
 /*
- * We need to fire at exit, so all shared libraries are still loaded,
- * so we will be able to resolve symbols.
+ * We use atexit() for consistency with _with_stacks variant.
  */
 static void
-init(void)
+init_trace(void)
 {
 	mprofile_init();
 	pthread_key_create(&mp_pthrd_key, merge_profile);
-	CRYPTO_set_mem_functions(mp_CRYPTO_malloc, mp_CRYPTO_realloc,
-	    mp_CRYPTO_free);
-	atexit(save_profile);
+	CRYPTO_set_mem_functions(mp_CRYPTO_malloc_trace,
+	    mp_CRYPTO_realloc_trace, mp_CRYPTO_free_trace);
+	atexit(save_profile_trace);
+}
+
+#ifdef _WITH_STACKTRACE
+/*
+ * We need to use at exit (instead of library destructor), so all shared
+ * libraries are still loaded, so we will be able to resolve symbols.
+ */
+static void
+init_trace_with_stacks(void)
+{
+	mprofile_init();
+	pthread_key_create(&mp_pthrd_key, merge_profile);
+	CRYPTO_set_mem_functions(mp_CRYPTO_malloc_trace_with_stack,
+	    mp_CRYPTO_realloc_trace_with_stack,
+	    mp_CRYPTO_free_trace_with_stack);
+	atexit(save_profile_trace);
+}
+
+#endif
+
+static void
+init(void)
+{
+	char *mprofile_mode = getenv("MPROFILE_MODE");
+	char default_mode[2] = { '1', 0 };
+
+	if (mprofile_mode == NULL)
+		mprofile_mode = default_mode;
+
+	switch (*mprofile_mode) {
+	case '1':
+		init_stats();
+		break;
+	case '2':
+		init_trace();
+		break;
+#ifdef _WITH_STACKTRACE
+	case '3':
+		init_trace_with_stacks();
+		break;
+#endif
+	default:
+		init_stats();
+	}
+}
+
+static void
+update_alloc(uint64_t delta)
+{
+	uint64_t current, max_current;
+	uint64_t max;
+
+	/* Perhaps ACQUIRE here should be sufficient */
+	current = __atomic_add_fetch(&ms.ms_current, delta, __ATOMIC_ACQ_REL);
+	max_current = __atomic_load_n(&ms.ms_max, __ATOMIC_ACQUIRE);
+	while (current > max_current)
+		__atomic_compare_exchange(&ms.ms_max, &max_current, &current,
+		    0/* weak */, __ATOMIC_RELEASE, __ATOMIC_ACQUIRE);
+}
+
+static void
+update_release(uint64_t delta)
+{
+	/* Perhaps ACQUIRE here should be sufficient */
+	__atomic_sub_fetch(&ms.ms_current, delta, __ATOMIC_ACQ_REL);
+}
+
+static void *
+mp_CRYPTO_malloc_stats(unsigned long sz, const char *f, int l)
+{
+	struct memhdr *mh;
+	void *rv;
+
+	mh = (struct memhdr *) malloc(sz + sizeof (struct memhdr));
+	if (mh != NULL) {
+		mh->mh_size = sz;
+		mh->mh_chk = (uint64_t)mh ^ sz;
+		rv = (void *)((char *)mh + sizeof (struct memhdr));
+	} else {
+		rv = NULL;
+	}
+
+	if (mh != NULL) {
+		/* RELAXED should be OK as we don't care about result here */
+		__atomic_add_fetch(&ms.ms_allocs, 1, __ATOMIC_RELAXED);
+		__atomic_add_fetch(&ms.ms_total_allocated, sz,
+		    __ATOMIC_RELAXED);
+		update_alloc(sz);
+	}
+
+	return (rv);
+}
+
+static void
+mp_CRYPTO_free_stats(void *b, const char *f, int l)
+{
+	struct memhdr *mh = NULL;
+	uint64_t chk;
+
+	if (b != NULL) {
+		mh = (struct memhdr *)((char *)b - sizeof (struct memhdr));
+		chk = (uint64_t)mh ^ mh->mh_size;
+		if (chk != mh->mh_chk) {
+			fprintf(stderr, "%p memory corruption detected in %s!",
+			    b, __func__);
+			abort();
+		}
+		/* RELAXED should be OK as we don't care about result here */
+		__atomic_add_fetch(&ms.ms_free, 1, __ATOMIC_RELAXED);
+		__atomic_add_fetch(&ms.ms_total_released, mh->mh_size,
+		    __ATOMIC_RELAXED);
+		update_release(mh->mh_size);
+	}
+
+	free(mh);
+}
+
+static void *
+mp_CRYPTO_realloc_stats(void *b, unsigned long sz, const char *f, int l)
+{
+	struct memhdr *mh;
+	struct memhdr save_mh;
+	uint64_t chk, delta;
+	void *rv = NULL;
+
+	if (b != NULL) {
+		mh = (struct memhdr *)((char *)b - sizeof (struct memhdr));
+		chk = (uint64_t)mh ^ mh->mh_size;
+		if (chk != mh->mh_chk) {
+			fprintf(stderr, "%p memory corruption detected in %s!",
+			    b, __func__);
+			abort();
+		}
+		save_mh = *mh;
+	} else {
+		mh = NULL;
+	}
+
+	if ((sz == 0) && (b != NULL)) {
+		/* this is realloc(x, 0); it's counted as free() */
+
+		/* RELAXED should be OK as we don't care about result here */
+		__atomic_add_fetch(&ms.ms_free, 1, __ATOMIC_RELAXED);
+		__atomic_add_fetch(&ms.ms_total_released, mh->mh_size,
+		    __ATOMIC_RELAXED);
+		update_release(mh->mh_size);
+	}
+
+	mh = (struct memhdr *)realloc(mh, (sz != 0) ?
+	    sz + sizeof (struct memhdr) : 0);
+	if (mh == NULL)
+		return (NULL);	/* consider recording failure */
+
+	if (sz == 0)
+		return ((mh == NULL) ?
+		    NULL : ((char *)mh) + sizeof (struct memhdr));
+
+	rv = (void *)((char *)mh + sizeof (struct memhdr));
+	if (mh != NULL) {
+		mh->mh_size = sz;
+		mh->mh_chk = (uint64_t)mh ^ sz;
+		if (b == NULL) {
+			/* this is  realloc(NULL, n); it's counted as malloc() */
+
+			/* RELAXED should be OK as we don't care about result here */
+			__atomic_add_fetch(&ms.ms_allocs, 1, __ATOMIC_RELAXED);
+			/* RELAXED should be OK as we don't care about result here */
+			__atomic_add_fetch(&ms.ms_total_allocated, sz,
+			    __ATOMIC_RELAXED);
+			update_alloc(sz);
+		} else {
+			__atomic_add_fetch(&ms.ms_reallocs, 1, __ATOMIC_RELAXED);
+			if (save_mh.mh_size > mh->mh_size) {
+				/* memory is shrinking */
+				delta = save_mh.mh_size - mh->mh_size;
+				update_release(delta);
+				__atomic_add_fetch(&ms.ms_total_released, delta,
+				    __ATOMIC_RELAXED);
+			} else {
+				/* memory is growing */
+				delta = save_mh.mh_size - mh->mh_size;
+				__atomic_add_fetch(&ms.ms_total_allocated, delta,
+				    __ATOMIC_RELAXED);
+				update_alloc(delta);
+			}
+		}
+	}
+
+	return (rv);
+}
+
+static void *
+mp_CRYPTO_malloc_trace(unsigned long sz, const char *f, int l)
+{
+	struct memhdr *mh;
+	void *rv;
+	mprofile_t *mp = get_mprofile();
+
+	mh = (struct memhdr *) malloc(sz + sizeof (struct memhdr));
+	if (mh != NULL) {
+		mh->mh_size = sz;
+		mh->mh_chk = (uint64_t)mh ^ sz;
+		rv = (void *)((char *)mh + sizeof (struct memhdr));
+	} else {
+		rv = NULL;
+	}
+
+	if (mp != NULL)
+		mprofile_record_alloc(mp, rv, sz, NULL);
+
+	return (rv);
+}
+
+static void
+mp_CRYPTO_free_trace(void *b, const char *f, int l)
+{
+	struct memhdr *mh = NULL;
+	mprofile_t *mp = get_mprofile();
+	uint64_t chk;
+
+	if (b != NULL) {
+		mh = (struct memhdr *)((char *)b - sizeof (struct memhdr));
+		chk = (uint64_t)mh ^ mh->mh_size;
+		if (chk != mh->mh_chk) {
+			fprintf(stderr, "%p memory corruption detected in %s!",
+			    b, __func__);
+			abort();
+		}
+	}
+
+	if (mp != NULL)
+		mprofile_record_free(mp, b, (b == NULL) ? 0 : mh->mh_size,
+		    NULL);
+	free(mh);
+}
+
+static void *
+mp_CRYPTO_realloc_trace(void *b, unsigned long sz, const char *f, int l)
+{
+	struct memhdr *mh;
+	struct memhdr save_mh;
+	uint64_t chk;
+	mprofile_t *mp = get_mprofile();
+	void *rv = NULL;
+
+	if (b != NULL) {
+		mh = (struct memhdr *)((char *)b - sizeof (struct memhdr));
+		chk = (uint64_t)mh ^ mh->mh_size;
+		if (chk != mh->mh_chk) {
+			fprintf(stderr, "%p memory corruption detected in %s!",
+			    b, __func__);
+			abort();
+		}
+		save_mh = *mh;
+	} else {
+		mh = NULL;
+	}
+
+	if (sz == 0)
+		mprofile_record_free(mp, b, (b == NULL) ? 0 : mh->mh_size, NULL);
+
+	mh = (struct memhdr *)realloc(mh, (sz != 0) ?
+	    sz + sizeof (struct memhdr) : 0);
+	if (mh == NULL)
+		return (NULL);	/* consider recording failure */
+
+	if (sz == 0)
+		return (b);
+
+	rv = (void *)((char *)mh + sizeof (struct memhdr));
+	if (mp != NULL) {
+		mh->mh_size = sz;
+		mh->mh_chk = (uint64_t)mh ^ sz;
+		if (b == NULL) {
+			mprofile_record_alloc(mp, rv, sz, NULL);
+		} else {
+			mprofile_record_realloc(mp, rv, sz,
+			    save_mh.mh_size, b, NULL);
+		}
+	}
+
+	return (rv);
 }
 
 #ifdef _WITH_STACKTRACE
@@ -127,21 +486,16 @@ collect_backtrace(struct _Unwind_Context *uw_context, void *cb_arg)
 	return (_URC_NO_REASON);
 }
 #endif	/* USE_LIBUNWIND */
-#endif
 
-void *
-mp_CRYPTO_malloc(size_t sz, const char *file, int line)
+static void *
+mp_CRYPTO_malloc_trace_with_stack(unsigned long sz, const char *f, int l)
 {
 	struct memhdr *mh;
 	void *rv;
 	mprofile_t *mp = get_mprofile();
-#ifdef _WITH_STACKTRACE
 	char stack_buf[512];
 	mprofile_stack_t *mps = mprofile_init_stack(stack_buf,
 	    sizeof (stack_buf));
-#else
-	mprofile_stack_t *mps = NULL;
-#endif
 
 	mh = (struct memhdr *) malloc(sz + sizeof (struct memhdr));
 	if (mh != NULL) {
@@ -151,40 +505,32 @@ mp_CRYPTO_malloc(size_t sz, const char *file, int line)
 	} else {
 		rv = NULL;
 	}
-#ifdef _WITH_STACKTRACE
 #ifdef USE_LIBUNWIND
 	collect_backtrace(mps);
 #else
 	_Unwind_Backtrace(collect_backtrace, mps);
 #endif
-#endif	/* _WITH_STACKTRACE */
 	if (mp != NULL)
 		mprofile_record_alloc(mp, rv, sz, mps);
 
 	return (rv);
 }
 
-void
-mp_CRYPTO_free(void *b, const char *file, int line)
+static void
+mp_CRYPTO_free_trace_with_stack(void *b, const char *f, int l)
 {
 	struct memhdr *mh = NULL;
 	mprofile_t *mp = get_mprofile();
 	uint64_t chk;
-#ifdef _WITH_STACKTRACE
 	char stack_buf[512];
 	mprofile_stack_t *mps = mprofile_init_stack(stack_buf,
 	    sizeof (stack_buf));
-#else
-	mprofile_stack_t *mps = NULL;
-#endif
 
-#ifdef _WITH_STACKTRACE
 #ifdef USE_LIBUNWIND
 	collect_backtrace(mps);
 #else
 	_Unwind_Backtrace(collect_backtrace, mps);
 #endif
-#endif	/* _WITH_STACKTRACE */
 
 	if (b != NULL) {
 		mh = (struct memhdr *)((char *)b - sizeof (struct memhdr));
@@ -201,21 +547,18 @@ mp_CRYPTO_free(void *b, const char *file, int line)
 	free(mh);
 }
 
-void *
-mp_CRYPTO_realloc(void *b, size_t sz, const char *file, int line)
+static void *
+mp_CRYPTO_realloc_trace_with_stack(void *b, unsigned long sz, const char *f,
+    int l)
 {
 	struct memhdr *mh;
 	struct memhdr save_mh;
 	uint64_t chk;
 	mprofile_t *mp = get_mprofile();
 	void *rv = NULL;
-#ifdef _WITH_STACKTRACE
 	char stack_buf[512];
 	mprofile_stack_t *mps = mprofile_init_stack(stack_buf,
 	    sizeof (stack_buf));;
-#else
-	mprofile_stack_t *mps = NULL;
-#endif
 
 	if (b != NULL) {
 		mh = (struct memhdr *)((char *)b - sizeof (struct memhdr));
@@ -241,13 +584,11 @@ mp_CRYPTO_realloc(void *b, size_t sz, const char *file, int line)
 	if (sz == 0)
 		return (b);
 
-#ifdef _WITH_STACKTRACE
 #ifdef USE_LIBUNWIND
 	collect_backtrace(mps);
 #else
 	_Unwind_Backtrace(collect_backtrace, mps);
 #endif
-#endif	/* _WITH_STACKTRACE */
 	rv = (void *)((char *)mh + sizeof (struct memhdr));
 	if (mp != NULL) {
 		mh->mh_size = sz;
@@ -262,3 +603,4 @@ mp_CRYPTO_realloc(void *b, size_t sz, const char *file, int line)
 
 	return (rv);
 }
+#endif /* _WITH_STACKTRACE */
